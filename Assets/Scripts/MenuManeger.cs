4c3a3b7ad85e0468436b172b2f7cfcf22e2a4e8a
muripoLife <life.is.too.high.difficulty@gmail.com>
2016-05-07 09:21:40 +0900

    menu初期追加

---
+++ Assets/Scripts/MenuManeger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManeger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void ButtonPush() {
+		Debug.Log("Button Push !!");
+
+	}
+
+	public void SceneLoad () {
+		
+	}
+
+}
